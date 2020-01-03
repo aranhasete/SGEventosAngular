b8650937872c328f6df57d31f405f20e6fabd70f
Diego Lima <diego.lima@supero.com.br>
2020-01-03 09:13:47 -0300

    Criado primeiro Model, adicionado e registrado Context do banco de dados

---
+++ SGEventosWebAPI/Models/EventoItem.cs
@@ -0,0 +1,9 @@
+namespace SGEventosWebAPI.Models
+{
+    public class EventoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
